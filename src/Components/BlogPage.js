6f10176d3eb488c05f7235f25820002e3ac6be04
Neemahj~ <neemahjinadu@gmail.com~>
2022-06-03 17:03:23 +0100

    Connected to API

--- src/Components/BlogPage.js
+++ src/Components/BlogPage.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogPosts } from "./Data"
+import axios from 'axios'
+
 
 const BlogPage = () => {
+  const [displayBlog, setDisplayBlog] = useState([]);
+
+  useEffect(() => {
+    const getPost = async() => {
+      const {data} = await axios.get("https://blogpostapi1.herokuapp.com/");
+      setDisplayBlog(data.data)
+    }
+    getPost();
+  }, []);
+  console.log(displayBlog);
   return (
     <div>
-      {BlogPosts.map((blogPost) =>{
+      {displayBlog.map((blogPost) =>{
         return(
           <div className="blog-content">
-            <p className="p-author">{blogPost.author}</p>
+            <p className="p-author">{blogPost.name}</p>
             <p className="p-title">{blogPost.title}</p>
-            <p className="p-body">{blogPost.body}</p>
-            <p className="p-comment">{blogPost.comment}</p>
+            <p className="p-body">{blogPost.content}</p>
+            {/* <p className="p-comment">{blogPost.comment}</p> */}
 
             <button className="submit-comment">Comment</button>
           </div>
--- src/Components/NewBlog.js
+++ src/Components/NewBlog.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const CreatePost = () =>{
+  const [name, setName] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [content, setContent] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("https://blogpostapi1.herokuapp.com/", {
+      name,
+      title,
+      content,
+      }
+    )
+  };
+  
   return(
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="field">
         <label for="author">Author:
-          <input type="text" id="author"/>
+          <input type="text" id="author" onChange={(e)=> setName(e.target.value)}/>
         </label>
       </div>
       <div className="field">
         <label for="title">Title:
-          <input type="text" id="title"/>
+          <input type="text" id="title" onChange={(e)=> setTitle(e.target.value)}/>
         </label>
       </div>
       <div className="body">
         <label for="body">Body:
-          <input type="text" id="body"/>
+          <input type="text" id="body" onChange={(e)=> setContent(e.target.value)}/>
         </label>
       </div>
       <button className="submit-post">Submit</button>
